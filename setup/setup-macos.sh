cf700813b20e5e533d630932ec1c5fe80a54e9b5
Hamza Elgaoussi <elgaoussi.hamza@gmail.com>
2021-12-13 11:50:39 +0100

    Update setup-macos

--- setup/setup-macos.sh
+++ setup/setup-macos.sh
@@ -56,6 +56,9 @@ SYMLINKS+=('.zshrc')
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
+# install tmp
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
 #==============
 # Set zsh as the default shell
 #==============
